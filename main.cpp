ddb28d62107aa8f51bd270d4fdf3052210ccf3c6
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-05-14 17:04:05 +0300

    client code cleaned

--- main.cpp
+++ main.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
-#include <GL/glut.h> 
 #include <vector>
-#include "pvector.h"
-#include "agent.h"
-#include "point.h"
-#include "color.h"
-#include "graphics.h"
-#include "obstacle.h"
-#include "path.h"
-#include "steeringBehavior.h"
 #include <stdlib.h>
-#include "random.h"
-
 #include "mouseFollower.h"
 #include "prison.h"
 #include "windy.h"
@@ -47,73 +36,8 @@ void menu(){
    cin >> mode;
 }
 
-void createStaticAgents(){
-    agent agent1 {-10.0,  0.0};
-    agent1.id = 1;
-    agent1.name = "gazelle";    
-    agent1.fillColor = myColor.getColor(BLUE);
-    agent1.setFeatures(0.5, 0.2, 5, 1);
-    agents.push_back(agent1);
-
-    agent agent2 { 10.0,  0.0};
-    agent2.id = 2;
-    agent2.name = "lion";
-    agent2.fillColor = myColor.getColor(YELLOW);
-    agent2.setFeatures(0.4, 0.2, 5, 1);    
-    agents.push_back(agent2);
-}
-
-void loop() {      
-   view.refreshScene();   
-   for(auto it = agents.begin(); it < agents.end(); it++){          
-      if(mode == AVOID_OBSTACLE){
-         for(auto it = obstacles.begin(); it < obstacles.end(); it++){
-            point p = (*it).p;
-            view.drawCircle(p, (*it).r);
-         }
-
-         (*it).targetPoint = view.getMousePosition();
-         pvector seek  = behavior.seek(*it);
-         seek.mul(0.5);
-
-         pvector avoid = behavior.avoid(obstacles, *it);
-         (*it).force = avoid + seek;   
-         (*it).arrive = true;
-      }
-   }
-
-   for(auto it = agents.begin(); it < agents.end(); it++){       
-      (*it).updatePosition((*it).arrive);        
-      view.drawAgent(*it, (*it).fillColor);
-   }
-      
-   view.drawText(scenario, point(-34, 32.25)); //TODO: magic numbers, define left corner
-}
-
-void createObstacle(vector<obstacle> &obstacles){   
-   obstacles.push_back(obstacle(point(0,0), 8));   
-   obstacles.push_back(obstacle(point(-20,0), 3));  
-   obstacles.push_back(obstacle(point(20,-10), 4));
-}
-
-void init(int * argv, char** argc, void (*callback)()){         
-   srand(time(NULL));
-   myColor.createColors();
-
-   if(mode == AVOID_OBSTACLE){
-      createStaticAgents();
-      createObstacle(obstacles);
-      scenario = "OBSTACLE AVOIDANCE";
-   }
-
-   view = graphics(); 
-   view.initGraphics(argv, argc, loop);
-}
-
-
 int main(int argc, char** argv) {    
    menu();  
-   //init(&argc, argv, loop); 
 
    if(mode == FOLLOW_MOUSE){
       mouseFollower mf;
