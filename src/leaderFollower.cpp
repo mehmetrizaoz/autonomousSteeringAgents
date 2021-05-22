3224756f1c04bc5640899c63c008aeb56471f83e
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-05-22 19:43:57 +0300

    parking behavior of leader following updated

--- src/leaderFollower.cpp
+++ src/leaderFollower.cpp
@@ -17,50 +17,48 @@ point leaderFollower::leaderPosition;
 
 void leaderFollower::loop()
 {
-    for(auto it = agents.begin(); it < agents.end(); it++){
+    int k=1;
+    int j=0;
+    int t=3;
+    point p = leaderPosition + leaderVelocity;
+
+    for(auto it = agents.begin(); it < agents.end(); it++){        
         if((*it).getName() == "leader"){
             (*it).setTarget(view.getMousePosition());
-            (*it).force  = behavior.seek(*it);  
-            leaderVelocity = (*it).getVelocity();  
+            (*it).force  = behavior.seek(*it);
+            leaderVelocity = (*it).getVelocity();
             leaderVelocity.mul(-1);
-            leaderVelocity.normalize().mul(20);
-            leaderPosition = (*it).position;
-            
+            leaderVelocity.normalize().mul(10);
+            leaderPosition = (*it).position;            
             view.drawText((*it).getName(), point(leaderPosition.x -3, leaderPosition.y - 3));
-
         }
-        else{            
+        else{
             pvector sep = behavior.separation(agents, *it, 3);
-            sep.mul(15);
+            sep.mul(20);
             (*it).force = sep;
 
-            (*it).setTarget(leaderPosition + leaderVelocity);
-            view.drawCircle((*it).getTarget(), 8, RED);                   
-
-            /*
-            pvector leaderDiff = (*it).position - leaderPosition;        
-            if(leaderDiff.magnitude() < 5){
-                pvector fle = behavior.evade(agents, *it, view, "leader");
-                fle.mul(40);
-                (*it).force += fle;
-            }*/
-
-            pvector diff = (*it).position - (*it).getTarget();
-            if(diff.magnitude() > 5) { 
-                (*it).force   += behavior.seek(*it); 
+            if(j==k){
+                k++;                
+                point pp = leaderPosition + leaderVelocity;
+                p.x = pp.x - t*(k-1);
+                p.y = p.y - t;
+                j=0;
             }
-            else{
-                (*it).setVelocity(pvector(0,0));
-            }
+            view.drawPoint(leaderPosition + leaderVelocity);
+            //view.drawPoint(p);
+            (*it).setTarget(p);
+            p.x = p.x + 2 * t; 
+            j++;
+            (*it).force += behavior.seek(*it);            
         }    
-        (*it).arrive = true;    
+        (*it).arrive = true;
     }            
     refresh();
 }
 
 leaderFollower::leaderFollower()
 {
-    int agentCount = 4;
+    int agentCount = 15;
     float maxForce = 0.4;
     float maxSpeed = 0.4;       
     name = "leader following";
@@ -68,7 +66,7 @@ leaderFollower::leaderFollower()
     //todo: refactor leader creation
     agent agent1 {-10.0,  0.0};
     agent1.id = 1;
-    agent1.setName("leader");    
+    agent1.setName("leader");
     agent1.setColor(BLUE);
     agent1.setFeatures(0.8, 0.4, 5, 1);
     agents.push_back(agent1);
