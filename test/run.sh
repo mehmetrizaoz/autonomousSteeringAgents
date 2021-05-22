534182d5fe13aa88751372c415739e0eea60b967
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-05-22 14:14:11 +0300

    sequence diagram updates

--- test/run.sh
+++ test/run.sh
@@ -13,11 +13,12 @@ echo "### static analysis result" > static_analysis.out
 cppcheck --enable=style ../src/ >> static_analysis.out
 echo "-------------" >> static_analysis.out
 cppcheck --enable=style ../main.cpp >> static_analysis.out 2>> static_analysis.out
+
 echo "-------------" >> static_analysis.out
-cppcheck --enable=style test_suites.cpp >> static_analysis.out
+cppcheck --enable=style test_suites.cpp >> static_analysis.out 2>> static_analysis.out
 echo "-------------" >> static_analysis.out
 
-echo "### unit tests result" > unit_test.out
-./test_suites >> unit_test.out
+echo "### unit tests result" > unit_test.out 2>> unit_test.out
+./test_suites >> unit_test.out 2>> unit_test.out
 
-echo "check test.out file"
+echo "check output files"
