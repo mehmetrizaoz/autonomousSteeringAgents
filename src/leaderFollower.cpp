83a977b3d96833bb1f5d1eff7061d675a8578bdb
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-05-23 13:18:56 +0300

    force enabled

--- src/leaderFollower.cpp
+++ src/leaderFollower.cpp
@@ -23,20 +23,12 @@ void leaderFollower::loop()
     int k=1;
     int j=0;
     int t=3;
-    point p1 = point(10,0);//leaderPosition + leaderVelocity;
+    point p1 = point(10,10);//leaderPosition + leaderVelocity;c
 
-    //point ppp = point(1,0);
-    //ppp.rotate(45);
-    //ppp.print("ee");
-    
-    /*pvector kss = pvector(5,0);
-    float dd = kss.getAngle();
-    cout << "dd" << dd << endl;*/
-    //cout << "sss" << endl;
     for(auto it = agents.begin(); it < agents.end(); it++){      
         if((*it).getName() == "leader"){
-            /*(*it).setTarget(view.getMousePosition());
-            (*it).force  = behavior.seek(*it);*/
+            (*it).setTarget(view.getMousePosition());
+            (*it).force  = behavior.seek(*it);
             leaderVelocity = (*it).getVelocity();
             leaderVelocity.mul(-1);
             leaderVelocity.normalize().mul(10);
@@ -47,13 +39,13 @@ void leaderFollower::loop()
             view.drawText((*it).getName(), point(leaderPosition.x -3, leaderPosition.y - 3));
         }
         else{
-            /*pvector sep = behavior.separation(agents, *it, 3);
+            pvector sep = behavior.separation(agents, *it, 3);
             sep.mul(15);
-            (*it).force = sep;*/
+            (*it).force = sep;
 
             if(j==k){
                 k++;                
-                point pp = point(10,0);//leaderPosition + leaderVelocity;
+                point pp = point(10,10);//leaderPosition + leaderVelocity;
                 p1.y = pp.y + t*(k-1);
                 p1.x = p1.x - t;
                 j=0;
@@ -63,8 +55,7 @@ void leaderFollower::loop()
            
             (*it).targetPoint = p1;
             view.drawPoint((*it).targetPoint, RED);
-            //(*it).force += behavior.seek(*it);
-            //p1.print("");
+
             p1.y = p1.y - 2 * t; 
             j++;
         }   
@@ -74,26 +65,26 @@ void leaderFollower::loop()
     for(auto it = agents.begin(); it < agents.end(); it++){
         if((*it).getName() == "leader"){}
         else{
-            cout << endl;
-            (*it).targetPoint.print("p");
+            //cout << endl;
+            //(*it).targetPoint.print("p");
             float diff = (10 - (*it).targetPoint.x) * (10 - (*it).targetPoint.x);
-            diff      += (0  - (*it).targetPoint.y) * (0  - (*it).targetPoint.y);
+            diff      += (10 - (*it).targetPoint.y) * (10  - (*it).targetPoint.y);
             diff       = sqrt(diff);
-            cout << "diff " << diff << endl;
+            //cout << "diff " << diff << endl;
             
-            pvector mm = (*it).targetPoint - point(10,0);
+            pvector mm = (*it).targetPoint - point(10,10);
             float ang = mm.getAngle();
-            cout << "ang" << ang << endl;
+            //cout << "ang" << ang << endl;
             
             (*it).targetPoint.x = diff * cos((ang + 45) * PI / 180);
             (*it).targetPoint.y = diff * sin((ang + 45) * PI / 180);
 
-            (*it).targetPoint = point(10, 0) + (*it).targetPoint;
-            (*it).targetPoint.print("pt");
+            (*it).targetPoint = point(10, 10) + (*it).targetPoint;
+            //(*it).targetPoint.print("pt");
 
             view.drawPoint((*it).targetPoint, BLUE);
 
-            //(*it).force += behavior.seek(*it);
+            (*it).force += behavior.seek(*it);
         }
     }
     cout << "---" << endl << endl;
@@ -102,7 +93,7 @@ void leaderFollower::loop()
 
 leaderFollower::leaderFollower()
 {
-    int agentCount = 3;
+    int agentCount = 6;
     float maxForce = 0.4;
     float maxSpeed = 0.4;       
     name = "leader following";
